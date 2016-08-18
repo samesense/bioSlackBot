d4f37882e8b5434c2e5af3c958fc091c88acf75e
evansj <evansj@email.chop.edu>
2016-08-18 16:03:10 -0400

    Load jc4 vars from hdf5 db.

--- genome-seq.py
+++ genome-seq.py
@@ -1,7 +1,7 @@
 import os
 import time
 from slackclient import SlackClient
-import dotenv, twobitreader
+import dotenv, twobitreader, tables
 
 #dotenv_path = '.env'
 dotenv.load()
@@ -17,16 +17,33 @@ AT_BOT = "<@" + BOT_ID + ">:"
 # instantiate Slack & Twilio clients
 slack_client = SlackClient(os.environ.get('SLACK_BOT_TOKEN'))
 
+def loadCalls(table, chrom, st, end):
+    """Load jc4 data.
+       Don't use chr
+    """
+    header = ('pos', 'ref', 'calls', 'depth', 'altFrac', 'zygosity')
+    resLs = [ '\t'.join([str(x['pos']),
+                         x['ref'].decode('UTF-8'),
+                         x['calls'].decode('UTF-8'),
+                         str(x['depth']),
+                         str(x['altFrac']),
+                         x['zygosity'].decode('UTF-8'),
+                         ]) for x in
+              table.where('(chrom == b"%s") & (pos >= %d) & (pos <= %d)'
+                          % (chrom, st, end)) ]
+    return '\n'.join(['\t'.join(header)] + resLs)
+
 def get_seq(tbFile, chrom, st, end):
     genome = twobitreader.TwoBitFile(tbFile)['chr' + chrom]
     return genome[st-1:end]
 
-def handle_command(command, channel):
+def handle_command(table, command, channel):
     """
         Receives commands directed at the bot and determines if they
         are valid commands. If so, then acts on the commands. If not,
         returns back what it needs for clarification.
     """
+    print('yo')
     #if command.startswith(EXAMPLE_COMMAND):
     genome, chrom, st, end = command.split(':')
     st, end = int(st), int(end)
@@ -34,11 +51,18 @@ def handle_command(command, channel):
     seq = get_seq(tbFile, chrom, st, end)
     slack_client.api_call("chat.postMessage", channel=channel,
                           text=seq, as_user=True)
-    r = slack_client.api_call("files.upload", channels=channel,
-                              content="ho ho ho", filename="test3.txt",
-                              as_user=True)
-    print('uploaded file')
-    print(r)
+    
+    if genome == 'jc4':
+        calls = loadCalls(table, chrom, st, end)
+        if calls:
+            content = calls
+        else:
+            content = 'no variants'
+        r = slack_client.api_call("files.upload", channels=channel,
+                                  content=calls, filename="test%s.txt" % (str(st),),
+                                  as_user=True)
+        print('uploaded file')
+        print(r)
 
 def parse_slack_output(slack_rtm_output):
     """
@@ -57,13 +81,17 @@ def parse_slack_output(slack_rtm_output):
 
 if __name__ == "__main__":
     READ_WEBSOCKET_DELAY = 1 # 1 second delay between reading from firehose
+    h5file = tables.open_file('/nas/is1/perry/projects/me/encode_genomes/data/vars/jc4.db', mode = "r")
+    table = h5file.root.posCollection.posLs
     if slack_client.rtm_connect():
         print("StarterBot connected and running!")
         #mm9TwoBitFile = 'data/mm9.2bit'
         while True:
             command, channel = parse_slack_output(slack_client.rtm_read())
             if command and channel:
-                handle_command(command, channel)
+                handle_command(table, command, channel)
             time.sleep(READ_WEBSOCKET_DELAY)
     else:
         print("Connection failed. Invalid Slack token or bot ID?")
+
+    h5file.close()
